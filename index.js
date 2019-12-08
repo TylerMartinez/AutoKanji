6fb7f7a9c24a0f54744142064259665521c32460
TylerMartinez <tyler.martinez89@gmail.com>
2019-12-07 22:49:43 -0500

    Initial Commit

---
+++ index.js
@@ -0,0 +1,12 @@
+// Functions
+const findMatches = function() {
+    console.log("Looking for matches");
+}
+
+// Setup api
+const api = {
+    find: findMatches
+} 
+
+// Export Module
+module.exports = api;
