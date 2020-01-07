be89e113e78f6b798bb060e52fb93053e9104140
TylerMartinez <tyler.martinez89@gmail.com>
2020-01-06 20:12:29 -0500

    Unziping and initializing of kanji trie

--- index.js
+++ index.js
@@ -1,23 +1,28 @@
 const fs = require('fs');
 const zlib = require('zlib');
 
-const fileContents = fs.createReadStream('./data/file1.txt.gz');
-
-console.log(fileContents);
 // Variables
 var Dictionary = null;
 
 // Functions
-const init = function() {
+const init = async function() {
+    // Setup streams
+    let fileContents = fs.createReadStream('./AutoKanjiTrie.json.gz');
+    let gunzip = zlib.createGunzip();
 
+    // Compile dictionary from zipped trie
+    let chunks = []
+    await fileContents.pipe(gunzip)
+        .on("data", (chunk) => {
+            chunks.push(chunk.toString());
+        })
+        .on("end", () => {
+            Dictionary = JSON.parse(chunks.join(''));
+        });
 };
 
 const findMatches = function() {
     console.log("Looking for matches");
-};
-
-const compileKanjiDictionary = function() {
-
 };
 
 // Setup api
